3186d684f517af6f07279b02f31975dc2122bf68
rhyanAnjos1 <rpyetro@icloud.com>
2026-06-03 08:51:35 -0300

    Erro de merge

--- BackEnd/src/main/java/br/com/hortitech/api/controllers/UsuarioController.java
+++ BackEnd/src/main/java/br/com/hortitech/api/controllers/UsuarioController.java
@@ -20,7 +20,7 @@ import br.com.hortitech.api.services.UsuarioServices;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/api/usuarios")
 // @CrossOrigin("*")
 public class UsuarioController {
 
